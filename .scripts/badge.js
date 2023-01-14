dc388a1752c9b2d371971aff727935add43e9a8d
Wes Reed <reedws@ucmail.uc.edu>
2023-01-14 12:17:24 -0500

    update scritps

--- .scripts/badge.js
+++ .scripts/badge.js
@@ -1,9 +1,9 @@
 const git = require(`simple-git`)(__dirname);
 const lineReplace = require(`line-replace`);
 
-async function main(){
+async function main() {
   const remote = await git.getRemotes(true);
-  const originRemote = remote.filter(r => r.name === `origin`)[0].refs.fetch.substring(4).slice(0,-4).replace(`:`,`/`);
+  const originRemote = remote.filter(r => r.name === `origin`)[0].refs.fetch.substring(4).slice(0, -4).replace(`:`, `/`);
 
   const result = `[![GitHub Classroom Workflow](https://${originRemote}/actions/workflows/classroom.yml/badge.svg)](https://${originRemote}/actions/workflows/classroom.yml)`;
 
@@ -11,13 +11,13 @@ async function main(){
     file: `README.md`,
     line: 3,
     text: result,
-    callback: ({file, line, text, replacedText, error}) => {
+    callback: ({ file, line, text, replacedText, error }) => {
       lineReplace({
         file: `README.md`,
         line: 8,
         text: `- [x] update the assignment checks above to the correct link. - Done Automatically`,
-        callback: ({file, line, text, replacedText, error}) => {
-          git.add([ `../README.md` ]);
+        callback: ({ file, line, text, replacedText, error }) => {
+          git.add([`../README.md`]);
           git.commit(`update README file`);
         }
       });
