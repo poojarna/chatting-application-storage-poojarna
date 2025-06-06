991d9b6f278da5bb653efb3d47773da38942fefa
Nirupama Poojari <poojarna@mail.uc.edu>
2025-06-06 19:50:34 -0400

    Update index.js

---
+++ resources/js/index.js
@@ -0,0 +1,4 @@
+const nameInput = document.getElementById("my-name-input");
+const myMessage = document.getElementById("my-message");
+const sendButton = document.getElementById("send-button");
+const chatBox = document.getElementById("chat");
