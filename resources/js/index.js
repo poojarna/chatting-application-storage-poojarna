44283625a2171d5f2949b3aa85560cb059ce5e7c
Nirupama Poojari <poojarna@mail.uc.edu>
2025-06-06 19:52:16 -0400

    Added updateMessages function

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,3 +2,11 @@ const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
+
+function updateMessages() {
+    // Fetch Messages
+    // Loop over the messages. Inside the loop we will
+        // get each message
+        // format it
+        // add it to the chatbox
+}
