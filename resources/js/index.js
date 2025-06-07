3ddc3d51f79b8cbd2c973a1e79cc0f8e6e9ea206
Nirupama Poojari <poojarna@mail.uc.edu>
2025-06-07 09:47:48 -0400

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,7 +2,7 @@
 const nameInput = document.getElementById("my-name-input");
 const messageInput = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
-const chatBox = document.getElementById("chat");
+const chatBox = document.querySelector(".chat");
 
 // ✅ 2. Define the server URL
 const serverURL = `https://it3049c-chat.fly.dev/messages`;
