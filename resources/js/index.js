1ba07b09aa7fd9f1c576cfafd7d6e19ab187d2cc
Nirupama Poojari <poojarna@mail.uc.edu>
2025-06-06 20:38:43 -0400

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -55,3 +55,19 @@ function formatMessage(message, myNameInput) {
 
 const MILLISECONDS_IN_TEN_SECONDS = 10000;
 setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
+
+function sendMessages(username, text) {
+    const newMessage = {
+        sender: username,
+        text: text,
+        timestamp: new Date()
+    }
+
+    fetch (serverURL, {
+        method: `POST`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newMessage)
+    });
+}
